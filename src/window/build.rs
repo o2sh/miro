47065322dc5fa1459ae7d6dc0b7c9a38ae28aaef
o2sh <ossama-hjaji@live.fr>
2021-03-22 18:18:30 +0100

    remove egl extensions

--- src/window/build.rs
+++ src/window/build.rs
@@ -11,28 +11,7 @@ fn main() {
     let target = env::var("TARGET").unwrap();
     if !target.contains("macos") {
         let mut file = File::create(&dest.join("egl_bindings.rs")).unwrap();
-        let reg = Registry::new(
-            Api::Egl,
-            (1, 5),
-            Profile::Core,
-            Fallbacks::All,
-            [
-                "EGL_KHR_create_context",
-                "EGL_EXT_create_context_robustness",
-                "EGL_KHR_create_context_no_error",
-                "EGL_KHR_platform_x11",
-                "EGL_KHR_platform_android",
-                "EGL_KHR_platform_wayland",
-                "EGL_KHR_platform_gbm",
-                "EGL_EXT_platform_base",
-                "EGL_EXT_platform_x11",
-                "EGL_MESA_platform_gbm",
-                "EGL_EXT_platform_wayland",
-                "EGL_EXT_platform_device",
-                "EGL_KHR_swap_buffers_with_damage",
-            ],
-        );
-
+        let reg = Registry::new(Api::Egl, (1, 5), Profile::Core, Fallbacks::All, []);
         reg.write_bindings(gl_generator::StructGenerator, &mut file).unwrap()
     }
 }
